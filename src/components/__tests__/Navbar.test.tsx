2de89ea4337297b355e7872d095ec084040cd7c1
Santosh <s.kunga.ganeshbabu@accenture.com>
2023-08-14 18:06:16 +0530

    updated test cases for navbar component

--- src/components/__tests__/Navbar.test.tsx
+++ src/components/__tests__/Navbar.test.tsx
@@ -1,27 +1,28 @@
-import { render, screen } from '@testing-library/react'
-import {MemoryRouter as Router} from 'react-router-dom';
+import { render, screen, fireEvent } from '@testing-library/react'
+import { MemoryRouter as Router, useLocation } from 'react-router-dom';
 
 import Navbar from '../Navbar';
 
+const handleHomeIconClick = jest.fn();
 describe('Navbar', () => {
     it('should render properly', () => {
         render(<Navbar />);
         expect(screen.getByTestId('home-icon')).toBeInTheDocument();
     })
 
-    it('should have title when title prop is passed',()=>{
+    it('should have title when title prop is passed', () => {
         render(<Navbar title={'DummyTitle'} />);
         const titleElement = screen.getByTestId('title');
         expect(titleElement).toBeInTheDocument();
     })
 
-    it('should not have title when no props passed', ()=>{
+    it('should not have title when no props passed', () => {
         render(<Navbar />);
         const titleElement = screen.queryByTestId('title');
         expect(titleElement).not.toBeInTheDocument();
     })
 
-    it('should have the searchbar when it is in homepage', ()=>{
+    it('should have the searchbar when it is in homepage', () => {
         render(
             <Router initialEntries={['/']}>
                 <Navbar />
@@ -30,14 +31,39 @@ describe('Navbar', () => {
         const searchbar = screen.getByTestId('search-bar');
         expect(searchbar).toBeInTheDocument();
     })
-    
-    it('should not have searcbar in other pages', ()=>{
+
+    it('should not have searcbar in other pages', () => {
         render(
-            <Router initialEntries={['/other']}>
+            <Router initialEntries={['/details']}>
                 <Navbar />
             </Router>
         )
         const searchbar = screen.queryByTestId('search-bar');
         expect(searchbar).not.toBeInTheDocument();
+    })
+
+    it('home icon should not perfrom any action in homepage', () => {
+        render(
+            <Router initialEntries={['/']}>
+                <Navbar />
+            </Router>
+        )
+        const homeIcon = screen.getByTestId('home-icon')
+        fireEvent.click(homeIcon)
+        expect(handleHomeIconClick).not.toBeCalled();
+    })
+
+    it('home button call the click function and navigate to the home route', () => {
+        const location = useLocation();
+        render(
+            <Router initialEntries={['/details']}>
+                <Navbar />
+            </Router>
+        )
+        const homeIcon = screen.getByTestId('home-icon')
+        fireEvent.click(homeIcon)
+        expect(handleHomeIconClick).toBeCalled();
+        const currentPathname = location.pathname;
+        expect(currentPathname).toBe('/');
     })
 })
