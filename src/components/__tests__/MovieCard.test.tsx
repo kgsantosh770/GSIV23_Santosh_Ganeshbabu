04e4932c2fd7de12cba5954647fb085cc7cf5945
Santosh <s.kunga.ganeshbabu@accenture.com>
2023-08-15 12:05:30 +0530

    removed unused imports

--- src/components/__tests__/MovieCard.test.tsx
+++ src/components/__tests__/MovieCard.test.tsx
@@ -1,5 +1,5 @@
 import '@testing-library/jest-dom'
-import { render, screen, fireEvent } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import MovieCard from '../MovieCard/MovieCard'
 import { MemoryRouter as Router } from 'react-router-dom'
 
